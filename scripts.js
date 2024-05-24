3803129de0f1a4a17c930fa18a4b856d7c359ce1
jek_a709 <evgenijblagun13@gmail.com>
2024-05-25 00:19:46 +0300

    upd

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// Функция для загрузки счета из localStorage
+function loadScore() {
+    const savedScore = localStorage.getItem('score');
+    return savedScore ? parseInt(savedScore, 10) : 0;
+}
+
+// Функция для сохранения счета в localStorage
+function saveScore(score) {
+    localStorage.setItem('score', score);
+}
+
+// Инициализация счета при загрузке страницы
+let score = loadScore();
+document.getElementById('score').innerText ='score: ' + score;
+
+// Добавление обработчика события клика на картинку
+document.getElementById('clickableImage').addEventListener('click', () => {
+    score += 1;
+    document.getElementById('score').innerText = 'score: ' + score;
+    saveScore(score);
+});
+
+// function clearStorage(){
+//     localStorage.clear();
+//     console.log('cleared');
+//     console.log(localStorage);
+// }
+
+function menu(){
+    document.getElementById('menu').style.display = "flex";
+}
